ce23e6d697dc818d6463ca3324a290b8b5c6594c
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2012-12-09 01:07:10 +1000

    Implemented rule 4

--- GameOfLife/Rules/DeadCellsWithThreeLiveNeighborsBecomeAlive.cs
+++ GameOfLife/Rules/DeadCellsWithThreeLiveNeighborsBecomeAlive.cs
@@ -4,21 +4,21 @@ using System.Linq;
 
 namespace Conway.GameOfLife
 {
-	public class LiveCellsWithTwoOrThreeLiveNeighborsLiveOn : IRule
+	public class DeadCellsWithThreeLiveNeighborsBecomeAlive : IRule
 	{
-		public IEnumerable<Cell> Apply (IEnumerable<Cell> cells)
+		public IEnumerable<Cell> Apply(IEnumerable<Cell> cells)
 		{
 			var liveCells = from cell in cells
-							where cell.State == State.Alive
+							where cell.State == State.Dead
 							let liveNeighborCount = cells.Count(otherCell => otherCell.Position.IsAdjacentTo(cell.Position)
-							                                                 && otherCell.State == State.Alive)
-							where liveNeighborCount == 2 || liveNeighborCount == 3
+								                                             && otherCell.State == State.Alive)
+							where liveNeighborCount == 3
 							select new Cell(cell.Position, State.Alive);
 			var otherCells = from cell in cells
 							 where !liveCells.Any(deadCell => deadCell.Position.Equals(cell.Position))
 							 select new Cell(cell.Position, cell.State);
 			return liveCells.Concat(otherCells);
-		}
+		}		
 	}
 }
 
